a3026912d6db22537fa8bd6553bd5bcacc70edbb
nikhilkhetan17 <nikhilkh1198@gmail.com>
2024-05-12 20:04:16 +0530

    arrow functions

--- 03-functions-scope/07-arrow-functions/script.js
+++ 03-functions-scope/07-arrow-functions/script.js
@@ -7,28 +7,27 @@
 const add = (a, b) => {
   return a + b;
 };
+console.log(add(1, 2));
 
 // Implicit Return
 const subtract = (a, b) => a - b;
+console.log(subtract(10, 5));
 
 // Can leave off () with a single param
 const double = (a) => a * 2;
+console.log(double(10));
 
-// Returning an object
+// Returning an object [surround it with ()]
 const createObj = () => ({
   name: 'Brad',
 });
+console.log(createObj());
 
 const numbers = [1, 2, 3, 4, 5];
 
-numbers.forEach(function (n) {
-  console.log(n);
-});
+// numbers.forEach(function (n) {
+//   console.log(n);
+// });
 
 // Arrow function in a callback
 numbers.forEach((n) => console.log(n));
-
-console.log(add(1, 2));
-console.log(subtract(10, 5));
-console.log(double(10));
-console.log(createObj());
