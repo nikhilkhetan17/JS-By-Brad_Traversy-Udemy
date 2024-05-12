7aa3611d408027ba6e98321171549f2d7702f799
nikhilkhetan17 <nikhilkh1198@gmail.com>
2024-05-12 18:23:41 +0530

    function declaration & Expression

--- 03-functions-scope/06-declaration-vs-expression/script.js
+++ 03-functions-scope/06-declaration-vs-expression/script.js
@@ -3,10 +3,11 @@ function addDollarSign(value) {
   return '$' + value;
 }
 
-// When using declarations, you can invoke the function before the declaration. With expressions, you can not
+// When using declarations, you can invoke the function before the declaration. With expressions, you cannot
 console.log(addDollarSign(100));
 
-// Function Expression
+// Function Expression (function assigned to a variable)
+// at the end of the expression we have a semicolon
 const addPlusSign = function (value) {
   return '+' + value;
 };
