98a25495e298d5f32bfe50dcfcbe9fffbb7d0182
nikhilkhetan17 <nikhilkh1198@gmail.com>
2024-05-16 17:12:37 +0530

    FIZZBUZZ

--- 05-iteration-array-methods/04-fizzbuzz-challenge/script.js
+++ 05-iteration-array-methods/04-fizzbuzz-challenge/script.js
@@ -11,31 +11,31 @@
 //   }
 // }
 
-// for (let i = 1; i <= 100; i++) {
-//   if (i % 3 === 0 && i % 5 === 0) {
-//     console.log(i + ' FIZZBUZZ');
-//   } else if (i % 5 === 0) {
-//     console.log(i + ' BUZZ');
-//   } else if (i % 3 === 0) {
-//     console.log(i + ' FIZZ');
-//   } else {
-//     console.log(i);
-//   }
-// }
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(i + ' FIZZBUZZ');
+  } else if (i % 5 === 0) {
+    console.log(i + ' BUZZ');
+  } else if (i % 3 === 0) {
+    console.log(i + ' FIZZ');
+  } else {
+    console.log(i);
+  }
+}
 
 // While Loop
 let j = 1;
 
-while (j <= 100) {
-  if (j % 15 === 0) {
-    console.log('FizzBuzz');
-  } else if (j % 3 === 0) {
-    console.log('Fizz');
-  } else if (j % 5 === 0) {
-    console.log('Buzz');
-  } else {
-    console.log(j);
-  }
+// while (j <= 100) {
+//   if (j % 15 === 0) {
+//     console.log('FizzBuzz');
+//   } else if (j % 3 === 0) {
+//     console.log('Fizz');
+//   } else if (j % 5 === 0) {
+//     console.log('Buzz');
+//   } else {
+//     console.log(j);
+//   }
 
-  j++;
-}
+//   j++;
+// }
