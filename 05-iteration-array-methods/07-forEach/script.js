c756e40702fdc885a9b2b92a8cc496aa890c4a80
nikhilkhetan17 <nikhilkh1198@gmail.com>
2024-05-16 23:34:02 +0530

    forEach

--- 05-iteration-array-methods/07-forEach/script.js
+++ 05-iteration-array-methods/07-forEach/script.js
@@ -1,3 +1,5 @@
+// They take in function as an argument which is called a callback function, in that callback function we can pass a variable for each element in that array.
+
 const socials = ['Twitter', 'LinkedIn', 'Facebook', 'Instagram'];
 
 // View prototype chain
@@ -9,10 +11,12 @@ socials.forEach(function (item) {
 });
 
 // Short form
-socials.forEach((item) => console.log(item));
+socials.forEach((element) => console.log(element));
 
 // We can also pass in the index and original array
-socials.forEach((item, index, arr) => console.log(`${index} - ${item}`, arr));
+socials.forEach((element, index, arr) =>
+  console.log(`${index} - ${element}`, arr)
+);
 
 // Using a named function
 function logSocials(social) {
@@ -30,3 +34,29 @@ const socialObjs = [
 ];
 
 socialObjs.forEach((item) => console.log(item.url));
+
+// --------------------------------------------------------
+
+// forEach doesn't return anything; it simply iterates over the array and applies the provided function to each element.
+// forEach method will modify/mutate the original array:
+
+// Original array
+let numbers = [1, 2, 3, 4, 5];
+
+// Using forEach to mutate the original array
+numbers.forEach((num, index) => {
+  numbers[index] = num * 2; // Modifying the original array
+});
+
+console.log(numbers); // Output: [2, 4, 6, 8, 10]
+
+// If you want to create a new array instead of modifying the original, you can use map.
+
+// Original array
+let numbers1 = [1, 2, 3, 4, 5];
+
+// Creating a new array without modifying the original
+let doubledNumbers = numbers.map((num) => num * 2);
+
+console.log(numbers1); // Output: [1, 2, 3, 4, 5]
+console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
