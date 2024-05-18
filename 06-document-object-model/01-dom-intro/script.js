ad04a72575805e098363d449e716638a705b1c6c
nikhilkhetan17 <nikhilkh1198@gmail.com>
2024-05-18 21:14:17 +0530

    dom intro

--- 06-document-object-model/01-dom-intro/script.js
+++ 06-document-object-model/01-dom-intro/script.js
@@ -3,10 +3,13 @@ console.log(window);
 
 // The document object is part of the window object
 console.dir(window.document);
+// console.log(window.document);
 
 // We can access DOM elements directly with properties
 console.log(document.body);
 console.log(document.links[0]);
+// console.log(document.body.innerText);
+// console.log(document.body.innerHTML);
 
 // We can set properties of elements
 // document.body.innerHTML = '<h1>Hello from body</h1>';
@@ -15,6 +18,7 @@ console.log(document.links[0]);
 document.write('Hello from JS');
 
 // We also have methods to select elements more directly
+console.log(document.getElementById('main'));
 document.getElementById('main').innerHTML = '<h1>Hello from main</h1>';
 
-document.querySelector('#main h1').innerText = 'Hello';
+// document.querySelector('#main h1').innerText = 'Hello';
