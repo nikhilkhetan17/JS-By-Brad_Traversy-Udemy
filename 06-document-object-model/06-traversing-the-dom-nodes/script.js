5538b39174d23b48f06563693823571f73ec771f
nikhilkhetan17 <nikhilkh1198@gmail.com>
2024-05-19 17:35:15 +0530

    general node

--- 06-document-object-model/06-traversing-the-dom-nodes/script.js
+++ 06-document-object-model/06-traversing-the-dom-nodes/script.js
@@ -1,12 +1,16 @@
 let output;
 
+// General Node - not used much just for reference
+
 // Get child nodes
 
 const parent = document.querySelector('.parent');
 
 output = parent.childNodes;
+console.log(output);
 output = parent.childNodes[0].textContent;
 output = parent.childNodes[0].nodeName;
+console.log(output);
 output = parent.childNodes[3].textContent;
 output = parent.childNodes[3].outerHTML;
 
@@ -15,12 +19,14 @@ output = parent.childNodes[5].style.color = 'red';
 
 output = parent.firstChild;
 output = parent.lastChild;
+console.log(output);
 
 parent.lastChild.textContent = 'Hello';
 
 // Get parent node
 
 const child = document.querySelector('.child');
+console.log(child);
 
 output = child.parentNode;
 output = child.parentElement;
