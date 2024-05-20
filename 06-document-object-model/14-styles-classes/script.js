ef42b9ce2fd902348bed186ca9bd3ab4a1867828
nikhilkhetan17 <nikhilkh1198@gmail.com>
2024-05-20 18:33:41 +0530

    style classes

--- 06-document-object-model/14-styles-classes/script.js
+++ 06-document-object-model/14-styles-classes/script.js
@@ -1,12 +1,15 @@
 const text = document.querySelector('p');
 const itemList = document.querySelector('.item-list');
 const items = itemList.querySelectorAll('li');
+// console.log(items);
 
 function run() {
   // className - Gets a string of all classes
   console.log(itemList.className);
+
   // Changing the classes with className
-  text.className = 'card dark';
+  // text.className = 'card dark';
+  // console.log(text);
 
   // classList - Array of classes, which also has methods to add, remove, toggle and replace
   console.log(itemList.classList);
@@ -16,9 +19,9 @@ function run() {
 
   // Add, remove, toggle, replace
   text.classList.add('dark');
-  text.classList.remove('card');
+  // text.classList.remove('card'); //having padding in style.css
   text.classList.toggle('hidden');
-  text.classList.replace('card', 'dark');
+  // text.classList.replace('card', 'dark');
 
   // style property - Add styles to elements
   itemList.style.lineHeight = '3';
