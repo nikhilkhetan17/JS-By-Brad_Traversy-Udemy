37eb54eb08138d041008adea986821304eb96871
nikhilkhetan17 <nikhilkh1198@gmail.com>
2024-05-21 19:56:16 +0530

    event object

--- 07-events/03-event-object/shopping-list/script.js
+++ 07-events/03-event-object/shopping-list/script.js
@@ -2,8 +2,9 @@ const logo = document.querySelector('img');
 
 function onClick(e) {
   // Event properties
-  // console.log(e.target);
-  // console.log(e.currentTarget);
+  console.log(e.target);
+  console.log(e.currentTarget);
+  // e.target.style.backgroundColor = 'black';
   // console.log(e.type);
   // console.log(e.timeStamp);
   // console.log(e.clientX);
@@ -18,21 +19,23 @@ function onClick(e) {
 
 function onDrag(e) {
   document.querySelector('h1').textContent = `X ${e.clientX} Y ${e.clientY}`;
+  // console.log('drag');
 }
 
 logo.addEventListener('click', onClick);
 logo.addEventListener('drag', onDrag);
 
 // document.body.addEventListener('click', function (e) {
+//   console.log(e);
 //   console.log(e.target);
 //   console.log(e.currentTarget);
 // });
 
 // e.preventDefault() method prevents the default behavior
-document.querySelector('a').addEventListener('click', function (e) {
-  e.preventDefault();
-  console.log('Link was clicked');
-});
+// document.querySelector('a').addEventListener('click', function (e) {
+//   e.preventDefault();
+//   console.log('Link was clicked');
+// });
 
 /*
 - `target` - The element that triggered the event
