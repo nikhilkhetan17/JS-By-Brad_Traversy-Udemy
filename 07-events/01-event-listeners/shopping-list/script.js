fadf7c90675eeb1a23e6464c173a0e7a70d47e67
nikhilkhetan17 <nikhilkh1198@gmail.com>
2024-05-21 15:18:21 +0530

    click event

--- 07-events/01-event-listeners/shopping-list/script.js
+++ 07-events/01-event-listeners/shopping-list/script.js
@@ -1,35 +1,40 @@
 const clearBtn = document.querySelector('#clear');
 
+// JavaScript Event Listener
+// clearBtn.onclick = function () {
+//   alert('Clear Items');
+// };
+
+// clearBtn.onclick = function () {
+//   console.log('Clear Items');
+// };
+
+// addEventListener()
+// clearBtn.addEventListener('click', function () {
+//   alert('Clear Items');
+// });
+clearBtn.addEventListener('click', () => console.log('Clear Items'));
+
 function onClear() {
   const itemList = document.querySelector('ul');
   const items = itemList.querySelectorAll('li');
 
-  // itemList.innerHTML = '';
+  // itemList.innerHTML = '';  //1st way
 
-  // items.forEach((item) => item.remove());
+  items.forEach((item) => item.remove()); // 2nd way
 
-  while (itemList.firstChild) {
-    itemList.removeChild(itemList.firstChild);
-  }
+  // while (itemList.firstChild) {
+  //   itemList.removeChild(itemList.firstChild); //3rd way
+  // }
+
+  // alert('Clear Items');
 }
-
-// JavaScript Event Listener
-clearBtn.onclick = function () {
-  alert('Clear Items');
-};
-
-clearBtn.onclick = function () {
-  console.log('Clear Items');
-};
-
-// addEventListener()
-clearBtn.addEventListener('click', () => alert('Clear Items'));
 
 // Use named function
 clearBtn.addEventListener('click', onClear);
 
 // removeEventListener()
-setTimeout(() => clearBtn.removeEventListener('click', onClear), 5000);
+setTimeout(() => clearBtn.removeEventListener('click', onClear), 5000); // removing event listner after 5 sec.
 
 // Fire off event from JS
-setTimeout(() => clearBtn.click(), 5000);
+// setTimeout(() => clearBtn.click(), 4000);
