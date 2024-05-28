a69fbfd2a9ced93a4ce0e846448b4a19de85113f
nikhilkhetan17 <nikhilkh1198@gmail.com>
2024-05-28 22:51:27 +0530

    prototypes

--- 13-oop-constructors-prototypes/05-prototypes/script.js
+++ 13-oop-constructors-prototypes/05-prototypes/script.js
@@ -1,3 +1,5 @@
+// A prototype is a special object where additional methods and properties can be attached and shared across all instances of its constructor function.
+
 function Rectangle(name, width, height) {
   this.name = name;
   this.width = width;
@@ -8,9 +10,11 @@ function Rectangle(name, width, height) {
 }
 
 const rect = new Rectangle('Rect', 10, 10);
+console.log(rect);
 
 //  Rectangle.prototype inherits from Object.prototype. That's why we can use toString(), etc
 console.log(rect.toString());
+console.log(Rectangle.prototype);
 
 // To get the prototype of an object
 console.log(Object.getPrototypeOf(rect));
